8a6777b867b5a87f90b79a4676d47b49fd7416c3
DavidKimDY <50807669+DavidKimDY@users.noreply.github.com>
2021-05-17 20:29:34 +0900

    Fix : Pause -> sleep

--- sync_and_async_with_request.py
+++ sync_and_async_with_request.py
@@ -6,7 +6,7 @@ from functools import partial
 
 # 1. 동기적 함수를 동기 방식으로 동작시키는 경우
 
-def sync_pause(text=None):
+def sync_sleep(text=None):
     print(f'{text} started')
     time.sleep(1)
     print(f'{text} ended')
@@ -16,7 +16,7 @@ def sync_pause(text=None):
 def sync_main():
     start = time.time()
     for i in range(10):
-        sync_pause(i)
+        sync_sleep(i)
     print(f'\n Taken time : {time.time() - start}')
 
 sync_main()
@@ -31,7 +31,7 @@ sync_main()
 
 # 2. 비동적 함수를 비동기 방식으로 동작시키는 경우
 
-async def async_pause(text):
+async def async_sleep(text):
     print(f'{text} started')
     await asyncio.sleep(1)
     print(f'{text} ended')
@@ -41,7 +41,7 @@ async def async_main():
     start = time.time()
     tasks = []
     for i in range(10):
-        task = asyncio.ensure_future(async_pause(i))
+        task = asyncio.ensure_future(async_sleep(i))
         tasks.append(task)
     await asyncio.gather(*tasks)
     print(f'\nTaken time : {time.time() - start}')
@@ -63,8 +63,8 @@ async def sync_to_async_main():
     tasks = []
     loop = asyncio.get_event_loop()
     for i in range(10):
-        pause = partial(sync_pause, text=i)
-        task = loop.run_in_executor(None, pause)
+        sleep = partial(sync_sleep, text=i)
+        task = loop.run_in_executor(None, sleep)
         tasks.append(task)
     await asyncio.gather(*tasks)
     print(f'\nTaken time : {time.time() - start}')
@@ -156,5 +156,4 @@ async def async_request(url_list):
 
 
 # asyncio.run(async_request(url_list))
-
 
